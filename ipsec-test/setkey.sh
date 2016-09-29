5211aa2a398bb829394dc0103131bd9af79f4eb5
Jehan <jehan@Jehans-MacBook.local>
2016-09-29 00:06:37 -0700

    more work on setkey script

--- ipsec-test/setkey.sh
+++ ipsec-test/setkey.sh
@@ -11,6 +11,10 @@ echo 'add 10.0.0.'$a' 10.0.0.'$var' ah 10'$a$var' -A hmac-md5 "1234567890123456"
 
 echo 'add 10.0.0.'$var' 10.0.0.'$a' ah 10'$var$a' -A hmac-md5 "1234567890123456";' | setkey -c
 
+echo 'spdadd 0.0.0.0/0 0.0.0.0/0 any -P out 1 discard;' | setkey -c
+
+echo 'spdadd 0.0.0.0/0 0.0.0.0/0 any -P in 1 discard;' | setkey -c
+
 echo 'spdadd 10.0.0.'$a' 10.0.0.'$var' any -P out ipsec ah/transport//require;' | setkey -c
 
 echo 'spdadd 10.0.0.'$var' 10.0.0.'$a' any -P in ipsec ah/transport//require;' | setkey -c
